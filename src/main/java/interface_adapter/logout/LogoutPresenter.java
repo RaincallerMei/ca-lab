43afc68922530c519e117578d80bb17d7b7feb0c
RohailAdnanCSC <rohailadnanca@gmail.com>
2025-10-21 12:52:56 -0400

    Logoutpresenter complete

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -1,10 +1,10 @@
 package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.logged_in.LoggedInState;
-import interface_adapter.login.LoginViewModel;
+import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
 import use_case.logout.LogoutOutputBoundary;
 import use_case.logout.LogoutOutputData;
 
@@ -14,9 +14,12 @@ import use_case.logout.LogoutOutputData;
 public class LogoutPresenter implements LogoutOutputBoundary {
 
     private final LoggedInViewModel loggedInViewModel;
-    private final ViewManagerModel viewManagerModel;
     private final LoginViewModel loginViewModel;
+    private final ViewManagerModel viewManagerModel;
 
+    /**
+     * Constructs the LogoutPresenter with the associated view models and view manager.
+     */
     public LogoutPresenter(ViewManagerModel viewManagerModel,
                            LoggedInViewModel loggedInViewModel,
                            LoginViewModel loginViewModel) {
@@ -25,19 +28,25 @@ public class LogoutPresenter implements LogoutOutputBoundary {
         this.loginViewModel = loginViewModel;
     }
 
+    /**
+     * Prepares the view for a successful logout.
+     * Clears the LoggedInState, updates the LoginState, and switches the view to login.
+     */
     @Override
     public void prepareSuccessView(LogoutOutputData response) {
+        // 1. Clear the LoggedInState
         LoggedInState loggedInState = loggedInViewModel.getState();
         loggedInState.setUsername("");
-        loggedInViewModel.setState(loggedInState);
-        loggedInViewModel.firePropertyChanged();
+        loggedInViewModel.firePropertyChange();
 
+        // 2. Update the LoginState with the username of the user who just logged out
         LoginState loginState = loginViewModel.getState();
         loginState.setUsername(response.getUsername());
-        loginViewModel.setState(loginState);
-        loginViewModel.firePropertyChanged();
+        loginState.setPassword("");  // optional: clear password field
+        loginViewModel.firePropertyChange();
 
-        this.viewManagerModel.setState(loginViewModel.getViewName());
-        this.viewManagerModel.firePropertyChange();
+        // 3. Switch to the login view
+        viewManagerModel.setState(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
     }
 }
